8666f578f504d7a2da9bec5095310f06fa3a9ac5
Tom Bevan <thehorrorthehorror@gmail.com>
2017-10-05 12:59:04 +0100

    updated event subjects (#9)

--- message.go
+++ message.go
@@ -135,7 +135,7 @@ func (m *Message) getServiceKey() string {
 // 'unsupported' will be returned as default value
 func (m *Message) getType() string {
 	switch m.subject {
-	case "service.create", "service.delete", "service.import", "service.patch":
+	case "build.create", "build.delete", "build.import", "build.patch":
 		return SERVICETYPE
 	}
 
